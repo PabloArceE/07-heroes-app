529dcdf50f39efc4527c4bc9a1bb2d2f5ed60e9b
Pablo Arce <arcepablo07@gmail.com>
2022-03-11 09:49:36 -0300

    Estilo del componente HeroScreen

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import getHeroById from "../../selectors/getHeroeById";
 
 const HeroScreen = () => {
@@ -6,15 +6,41 @@ const HeroScreen = () => {
 
   const hero = getHeroById(heroId);
 
-  if(!hero) {
-      return <Navigate to='/' />
-  } 
+  const navigate = useNavigate()
+
+  const handleReturn = () => {    
+    navigate (-1)
+  }
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+  const { id, superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+  const imgPath = `/assets/${id}.jpg`;
 
   return (
-    <>
-      <h1>Hero Screen</h1>
-      <p>{hero.superhero}</p>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={imgPath} alt={superhero} className="img-thumbnail" />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"> <b>Alter ego</b> {alter_ego} </li>
+          <li className="list-group-item"> <b>Publisher</b> {publisher} </li>
+          <li className="list-group-item"> <b>First appearance</b> {first_appearance} </li>
+        </ul>
+        <h5 className="mt-5">Characters</h5>
+        <p>{characters}</p>
+        <button
+            className="btn btn-outline-info"
+            onClick={handleReturn}
+        >
+            Regresar
+        </button>
+      </div>
+    </div>
   );
 };
 
