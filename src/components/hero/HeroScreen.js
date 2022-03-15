dd2c4fe54674a91ef3cf3b782399634e00fa35fc
Pablo Arce <arcepablo07@gmail.com>
2022-03-15 09:34:13 -0300

    I applied useMemo in getHeroesByPublisher and getHeroById functions

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,16 +1,16 @@
+import { useMemo } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import getHeroById from "../../selectors/getHeroeById";
 
 const HeroScreen = () => {
   const { heroId } = useParams();
+  const navigate = useNavigate();
 
-  const hero = getHeroById(heroId);
+  const hero = useMemo(() => getHeroById(heroId), [heroId]);
 
-  const navigate = useNavigate()
-
-  const handleReturn = () => {    
-    navigate (-1)
-  }
+  const handleReturn = () => {
+    navigate(-1);
+  };
 
   if (!hero) {
     return <Navigate to="/" />;
@@ -27,17 +27,23 @@ const HeroScreen = () => {
       <div className="col-8">
         <h3>{superhero}</h3>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item"> <b>Alter ego</b> {alter_ego} </li>
-          <li className="list-group-item"> <b>Publisher</b> {publisher} </li>
-          <li className="list-group-item"> <b>First appearance</b> {first_appearance} </li>
+          <li className="list-group-item">
+            {" "}
+            <b>Alter ego</b> {alter_ego}{" "}
+          </li>
+          <li className="list-group-item">
+            {" "}
+            <b>Publisher</b> {publisher}{" "}
+          </li>
+          <li className="list-group-item">
+            {" "}
+            <b>First appearance</b> {first_appearance}{" "}
+          </li>
         </ul>
         <h5 className="mt-5">Characters</h5>
         <p>{characters}</p>
-        <button
-            className="btn btn-outline-info"
-            onClick={handleReturn}
-        >
-            Regresar
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Regresar
         </button>
       </div>
     </div>
--- src/components/hero/HeroesList.js
+++ src/components/hero/HeroesList.js
@@ -1,19 +1,18 @@
+import { useMemo } from "react";
 import getHeroesByPublisher from "../../selectors/getHeroesByPublisher";
 import HeroCard from "./HeroCard";
 
 export const HeroesList = ({ publisher }) => {
-  const heroesList = getHeroesByPublisher(publisher);
+  const heroesList = useMemo(
+    () => getHeroesByPublisher(publisher),
+    [publisher]
+  );
 
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
-      {
-        heroesList.map((hero) => (
-          <HeroCard
-            key={hero.id}
-            {...hero} 
-          />
-        ))
-      }
+      {heroesList.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
     </div>
   );
 };
