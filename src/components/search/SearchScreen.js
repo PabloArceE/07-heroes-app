2947bebd06334d4bbf25037c5c76c4c65b35dad4
Pablo Arce <arcepablo07@gmail.com>
2022-03-17 09:49:06 -0300

    SearchComponent / useForm

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,10 +1,48 @@
+import useForm from "../../hooks/useForm";
 
 const SearchScreen = () => {
-    return(
-    <>
-        <h1>LoginScreen</h1>
-    </>
-    ) 
-}
+  const [values, handleInputChange, reset] = useForm({
+    searchText: "",
+  });
+  const { searchText } = values;
+  const hanldeSearch = (e) => {
+    e.preventDefault(searchText);
+    console.log(searchText)
+    reset();
+  };
 
-export default SearchScreen
+  return (
+    <>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Buscar</h4>
+          <hr />
+
+          <form>
+            <input
+              type="text"
+              placeholder="Buscar un heroe"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
+            />
+            <button
+              type="submit"
+              className="btn btn-outline-primary mt-3"
+              onClick={hanldeSearch}
+            >
+              Buscar
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchScreen;
--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -35,6 +35,14 @@ const NavBar = () => {
           >
             DC
           </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "nav-item nav-link active" : "nav-item nav-link"
+            }
+            to="/search"
+          >
+            Search
+          </NavLink>
         </div>
       </div>
 
